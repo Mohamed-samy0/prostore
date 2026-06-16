0e7483820b099d3e312f96e957dfb985561c3ec0
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-06-16 22:36:51 +0300

    first commit

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import { APP_Name } from "@/lib/constants";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="border-t">
+      <div className="p-5 flex-center">
+        {currentYear} {APP_Name}. All Rights reserved.{" "}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/shared/header/index.tsx
@@ -0,0 +1,28 @@
+import { APP_Name } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "./menu";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="./images/logo.svg"
+              alt={`${APP_Name} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold ml-3 text-2xl">{APP_Name}</span>
+          </Link>
+        </div>
+        <Menu />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
