5c797d7a37d0d5434cbf3b926402555bc94e6697
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-07-01 15:47:26 +0300

    Callback URL redirect

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -6,6 +6,7 @@ import { Label } from "@/components/ui/label";
 import { signInWithCredentials } from "@/lib/actions/user.actions";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { useActionState } from "react";
 import { useFormStatus } from "react-dom";
 
@@ -19,6 +20,9 @@ const SignInButton = () => {
 };
 
 const CredentialsSignInForm = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
   const [data, action] = useActionState(signInWithCredentials, {
     success: false,
     message: "",
@@ -26,6 +30,7 @@ const CredentialsSignInForm = () => {
 
   return (
     <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -52,11 +57,9 @@ const CredentialsSignInForm = () => {
         <div>
           <SignInButton />
         </div>
-
         {data && !data.success && (
           <div className="text-center text-destructive">{data.message}</div>
         )}
-
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
           <Link target="_self" className="link" href="/sign-up">
