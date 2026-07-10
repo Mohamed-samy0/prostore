d8d1bf52e6d5c40780bf6920afafc8714819e940
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-07-10 21:23:15 +0300

    Payment method and Zod schemas

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,17 @@
 "use server";
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from "../validators";
+import {
+  paymentMethodSchema,
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { prisma } from "@/db/prisma";
 import { hashSync } from "bcrypt-ts-edge";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
+import z from "zod";
 
 // Sign in the user with credentials
 
@@ -108,5 +114,27 @@ export async function updateUserAddress(data: ShippingAddress) {
       success: false,
       message: formatError(error),
     };
+  }
+}
+
+// update User Paymen tMethod
+export async function updateUserPaymentMethod(data: z.infer<typeof paymentMethodSchema>) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+    if (!currentUser) throw new Error("User not found");
+    const paymentMethod = paymentMethodSchema.parse(data);
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
