cb1c3eb5e3dba4e0dac6518c693703249af26117
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-07-22 15:28:25 +0300

    Orders pagination

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/shared/pagination";
 import {
   Table,
   TableBody,
@@ -60,6 +61,9 @@ const OrdersPage = async (props: { searchParams: Promise<{ page: string }> }) =>
             ))}
           </TableBody>
         </Table>
+        {orders.totalPages > 1 && (
+          <Pagination page={Number(page) || 1} totalPages={orders.totalPages} />
+        )}
       </div>
     </div>
   );
