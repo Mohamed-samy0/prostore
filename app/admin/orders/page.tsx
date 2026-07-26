f2a5f351daaa1d620a8832ad05e509a3b53df888
Mohmaed samy <156313969+Mohamed-samy0@users.noreply.github.com>
2026-07-27 00:51:33 +0300

    Delete orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import DeleteDialog from "@/components/shared/delete-dialog";
 import Pagination from "@/components/shared/pagination";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,7 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { getAllOrders } from "@/lib/actions/order.actions";
+import { deleteOrder, getAllOrders } from "@/lib/actions/order.actions";
 import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -28,7 +29,6 @@ const OrdersPage = async (props: { searchParams: Promise<{ page: string }> }) =>
     page: Number(page),
   });
 
-  console.log(orders);
   return (
     <div className="space-y-2">
       <h2 className="h2-bold">Orders</h2>
@@ -64,7 +64,7 @@ const OrdersPage = async (props: { searchParams: Promise<{ page: string }> }) =>
                   <Button asChild variant="outline" size="sm">
                     <Link href={`/order/${order.id}`}>Details</Link>
                   </Button>
-                  {/* DELETE */}
+                  <DeleteDialog id={order.id} action={deleteOrder} />
                 </TableCell>
               </TableRow>
             ))}
