be146a6c8abe0bc066c0556bc940d95f01eb02ca
Mohmaed samy <156313969+Mohamed-samy0@users.noreply.github.com>
2026-07-27 23:05:10 +0300

    Get products for admin page (#9)

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 import { convertToPlainObject } from "../utils";
-import { LATEST_PRODUCT_LIMIT } from "../constants";
+import { LATEST_PRODUCT_LIMIT, PAGE_SIZE } from "../constants";
 import { prisma } from "@/db/prisma";
 
 export async function getLatestProducts() {
@@ -18,4 +18,28 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug },
   });
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (1 - page) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
