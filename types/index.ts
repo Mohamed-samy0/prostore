c9167a6395ee2fdd3c86b8cbeba14ccd664fcf09
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-07-28 19:36:31 +0300

    Create and update actions

--- types/index.ts
+++ types/index.ts
@@ -30,3 +30,18 @@ export type Order = z.infer<typeof insertOrderSchema> & {
 };
 
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
+export const productDefaultValues = {
+  name: "",
+  slug: "",
+  category: "",
+  images: [],
+  brand: "",
+  description: "",
+  price: "0",
+  stock: 0,
+  rating: "0",
+  numReviews: "0",
+  isFeatured: false,
+  banner: null,
+};
