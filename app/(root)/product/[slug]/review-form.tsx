8615fa68c30f116bc3bcec028cd7eab3269aad65
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-08-11 01:22:27 +0300

    Connect review form to action

--- app/(root)/product/[slug]/review-form.tsx
+++ app/(root)/product/[slug]/review-form.tsx
@@ -25,12 +25,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { createUpdateReview } from "@/lib/actions/review.actions";
 import { reviewFormDefaultValues } from "@/lib/constants";
 import { insertReviewSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { StarIcon } from "lucide-react";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 const ReviewForm = ({
@@ -49,9 +51,26 @@ const ReviewForm = ({
     defaultValues: reviewFormDefaultValues,
   });
 
+  // Open Form Handler
   const handleOpenForm = () => {
+    form.setValue("productId", productId);
+    form.setValue("userId", userId);
+
     setOpen(true);
   };
+
+  // Submit form Handler
+  const onSubmit: SubmitHandler<z.infer<typeof insertReviewSchema>> = async (values) => {
+    const res = await createUpdateReview({ ...values, productId });
+    if (!res.success) {
+      toast.error(res.message);
+    }
+
+    setOpen(false);
+    onReviewSubmitted?.();
+    toast.success(res.message);
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <Button onClick={handleOpenForm} variant="default">
@@ -59,7 +78,7 @@ const ReviewForm = ({
       </Button>
       <DialogContent className="sm:max-w-[425px]">
         <Form {...form}>
-          <form method="post">
+          <form method="post" onSubmit={form.handleSubmit(onSubmit)}>
             <DialogHeader>
               <DialogTitle>Write a review</DialogTitle>
               <DialogDescription>
