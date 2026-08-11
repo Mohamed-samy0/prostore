46fa7e9d110865a43b0e54f2d0a8b1ee8cb0f415
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-08-11 23:20:37 +0300

    Webhook to mark order as paid

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -178,7 +178,7 @@ export async function approvePaypalOrder(orderId: string, data: { orderID: strin
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
